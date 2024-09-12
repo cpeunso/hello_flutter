1afa5df26fa6e3c6bd9855aaf62908bbe3a30ecb
cpeunso <cpeunso@gmail.com>
2024-09-12 20:29:44 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            Text(
+              "Hello flutter",
+              style: TextStyle(
+                fontSize: 28,
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "이메일",
+              ),
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "비밀번호",
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("로그인"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
